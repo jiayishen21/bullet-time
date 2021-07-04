26826d47ec2426b36a04f8832e8726de72e3d9b4
Lucas Shen <lucas.shxn@gmail.com>
2021-07-03 21:03:18 -0400

    facing direction

--- Bullet Time/Assets/Player.cs
+++ Bullet Time/Assets/Player.cs
@@ -8,12 +8,30 @@ public class Player : MonoBehaviour
 	public float moveSpeed = 6f;
 	public float midAirControl = 3f;
 	private Rigidbody2D rigidbody2d;
-	[SerializeField] private LayerMask platformLayerMask = LayerMask.GetMask("Platform");
+	public LayerMask platformLayerMask;
 	private BoxCollider2D boxCollider2d;
+	private bool isFacingLeft;
+	public bool spawnFacingLeft = false;
+	private Vector2 facingLeft;
+	private Vector2 facingRight;
 	void Start()
 	{
 		rigidbody2d = transform.GetComponent<Rigidbody2D>();
 		boxCollider2d = transform.GetComponent<BoxCollider2D>();
+		facingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
+		facingRight = new Vector2(transform.localScale.x, transform.localScale.y);
+		platformLayerMask = LayerMask.GetMask("Platform");
+
+		if (spawnFacingLeft)
+		{
+			transform.localScale = facingLeft;
+			isFacingLeft = true;
+		}
+		else
+		{
+			transform.localScale = facingRight;
+			isFacingLeft = false;
+		}
 	}
 
 	void Update()
@@ -30,10 +48,27 @@ public class Player : MonoBehaviour
 	{
 	}
 
+	private void Flip()
+	{
+		if (isFacingLeft)
+		{
+			transform.localScale = facingRight;
+		}
+		else
+		{
+			transform.localScale = facingLeft;
+		}
+		isFacingLeft = !isFacingLeft;
+	}
+
 	private void handleMovement()
 	{
 		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 		{
+			if (!isFacingLeft)
+			{
+				Flip();
+			}
 			if (IsGrounded())
 			{
 				rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
@@ -46,6 +81,10 @@ public class Player : MonoBehaviour
 		}
 		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 		{
+			if (isFacingLeft)
+			{
+				Flip();
+			}
 			if (IsGrounded())
 			{
 				rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
